5d3b6070172fc4f111b53919aaba3abd1a38a1d5
Andrey Popov <info@java4you.ru>
2022-11-09 23:22:05 +0300

    Refactor home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,6 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:jedi_pixels_products/services/connection_service.dart';
+import 'package:jedi_pixels_products/services/product/product_list_service.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -10,6 +10,46 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  late ConnectionService connectionService;
+
+  //late ProductService  productService;
+  final ProductListService productListService = ProductListService();
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    //AFTER....
+    connectionService = ConnectionService(productListService);
+    connectionService.watchConnectivity(productListService);
+    //Check if scroll has reached the bottom, then retrieve the next 10 records/products
+    scrollController.addListener(() {
+      if (scrollController.offset ==
+              scrollController.position.maxScrollExtent &&
+          !productListService.isProductLoading) {
+        //TODO: Get products... the next 10 products
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    //BEFORE...
+    super.dispose();
+  }
+
+  Future<bool> checkInternetConnection() async {
+    productListService.internetConnectionAvailability =
+        await connectionService.isInternetConnectionAvailable();
+    if (!productListService.internetConnectionAvailability) {
+      productListService.internetConnectionAvailability = false;
+      productListService.isProductLoading = false;
+      productListService
+          .addProductError('Internet connection is currently not available');
+    }
+    return productListService.internetConnectionAvailability;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +57,16 @@ class _HomeState extends State<Home> {
         title: const Text('Products'),
       ),
       body: SafeArea(
-        child:Container(),
+        child: StreamBuilder(
+          initialData: const [],
+          stream: productListService.getProductList,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (!productListService.internetConnectionAvailability) {
+              return const Text('No internet ...');
+            }
+            return const Text('No internet ...');
+          },
+        ),
       ),
     );
   }
