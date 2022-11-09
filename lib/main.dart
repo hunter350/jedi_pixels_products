1f092cdf31ce86823f578a1218b466f73f00aa5c
Andrey Popov <info@java4you.ru>
2022-11-09 03:06:36 +0300

    Models create

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'pages/home.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Home(),
     );
   }
 }
---
+++ lib/models/auth/auth_model.dart
@@ -0,0 +1,49 @@
+import 'dart:convert';
+
+class AuthModel {
+  AuthModel({
+    required this.id,
+    required this.username,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    required this.gender,
+    required this.image,
+    required this.token,
+  });
+
+  int id;
+  String username;
+  String email;
+  String firstName;
+  String lastName;
+  String gender;
+  String image;
+  String token;
+
+  factory AuthModel.fromRawJson(String str) => AuthModel.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory AuthModel.fromJson(Map<String, dynamic> json) => AuthModel(
+    id: json["id"],
+    username: json["username"],
+    email: json["email"],
+    firstName: json["firstName"],
+    lastName: json["lastName"],
+    gender: json["gender"],
+    image: json["image"],
+    token: json["token"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "username": username,
+    "email": email,
+    "firstName": firstName,
+    "lastName": lastName,
+    "gender": gender,
+    "image": image,
+    "token": token,
+  };
+}
