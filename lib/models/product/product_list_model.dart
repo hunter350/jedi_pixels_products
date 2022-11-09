fe40a092fc8acc7a11389cae0f40cb5fa88dee33
Andrey Popov <info@java4you.ru>
2022-11-09 17:14:18 +0300

    Product List Model part3

--- lib/models/product/product_list_model.dart
+++ lib/models/product/product_list_model.dart
@@ -1,61 +1,82 @@
 import 'dart:convert';
+import 'product_model.dart';
 
-class ProductListModel {
-  ProductListModel({
-    required this.id,
-    required this.title,
-    required this.description,
-    required this.price,
-    required this.discountPercentage,
-    required this.rating,
-    required this.stock,
-    required this.brand,
-    required this.category,
-    required this.thumbnail,
-    required this.images,
+class ProductsListModel {
+  ProductsListModel({
+    required this.products,
+    required this.total,
+    required this.skip,
+    required this.limit,
   });
 
-  int id;
-  String title;
-  String description;
-  int price;
-  double discountPercentage;
-  double rating;
-  int stock;
-  String brand;
-  String category;
-  String thumbnail;
-  List<String> images;
+  List<ProductModel> products;
+  int total;
+  int skip;
+  int limit;
 
-  factory ProductListModel.fromRawJson(String str) => ProductListModel.fromJson(json.decode(str));
+  factory ProductsListModel.fromRawJson(String str) => ProductsListModel.fromJson(json.decode(str));
 
   String toRawJson() => json.encode(toJson());
 
-  factory ProductListModel.fromJson(Map<String, dynamic> json) => ProductListModel(
-    id: json["id"],
-    title: json["title"],
-    description: json["description"],
-    price: json["price"],
-    discountPercentage: json["discountPercentage"].toDouble(),
-    rating: json["rating"].toDouble(),
-    stock: json["stock"],
-    brand: json["brand"],
-    category: json["category"],
-    thumbnail: json["thumbnail"],
-    images: List<String>.from(json["images"].map((x) => x)),
+  factory ProductsListModel.fromJson(Map<String, dynamic> json) => ProductsListModel(
+    products: List<ProductModel>.from(json["products"].map((x) => ProductModel.fromJson(x))),
+    total: json["total"],
+    skip: json["skip"],
+    limit: json["limit"],
   );
 
   Map<String, dynamic> toJson() => {
-    "id": id,
-    "title": title,
-    "description": description,
-    "price": price,
-    "discountPercentage": discountPercentage,
-    "rating": rating,
-    "stock": stock,
-    "brand": brand,
-    "category": category,
-    "thumbnail": thumbnail,
-    "images": List<dynamic>.from(images.map((x) => x)),
+    "products": List<dynamic>.from(products.map((x) => x.toJson())),
+    "total": total,
+    "skip": skip,
+    "limit": limit,
   };
 }
+
+// JSON
+/*
+{
+  "products": [
+    {
+      "id": 1,
+      "title": "iPhone 9",
+      "description": "An apple mobile which is nothing like apple",
+      "price": 549,
+      "discountPercentage": 12.96,
+      "rating": 4.69,
+      "stock": 94,
+      "brand": "Apple",
+      "category": "smartphones",
+      "thumbnail": "https://dummyjson.com/image/i/products/1/thumbnail.jpg",
+      "images": [
+        "https://dummyjson.com/image/i/products/1/1.jpg",
+        "https://dummyjson.com/image/i/products/1/2.jpg",
+        "https://dummyjson.com/image/i/products/1/3.jpg",
+        "https://dummyjson.com/image/i/products/1/4.jpg",
+        "https://dummyjson.com/image/i/products/1/thumbnail.jpg"
+      ]
+    },
+    {
+      "id": 2,
+      "title": "iPhone X",
+      "description": "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+      "price": 899,
+      "discountPercentage": 17.94,
+      "rating": 4.44,
+      "stock": 34,
+      "brand": "Apple",
+      "category": "smartphones",
+      "thumbnail": "https://dummyjson.com/image/i/products/2/thumbnail.jpg",
+      "images": [
+        "https://dummyjson.com/image/i/products/2/1.jpg",
+        "https://dummyjson.com/image/i/products/2/2.jpg",
+        "https://dummyjson.com/image/i/products/2/3.jpg",
+        "https://dummyjson.com/image/i/products/2/thumbnail.jpg"
+      ]
+    }
+  ],
+  "total": 100,
+  "skip": 0,
+  "limit": 30
+}
+*/
